2de1768916671f3bdd46a361c5d52365deff138a
Andre Ferrer <andrerferrer@hotmail.com>
2022-03-01 12:54:20 +0100

    improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 puts 'seed it 🌱'
 order = Order.create!
 
-banana = Item.create! name: 'banana 🍌', price_cents: 100
-croissant = Item.create! name: 'banana 🥐', price_cents: 250
+banana = Item.create! name: '🍌 Banana', price_cents: 100
+croissant = Item.create! name: '🥐 Croissant', price_cents: 250
 
 ItemOrder.create! item: banana, order: order, quantity: 1
 ItemOrder.create! item: croissant, order: order, quantity: 2
